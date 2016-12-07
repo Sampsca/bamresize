8b4c5079db0373c4e2c88da56830256b75a27ff6
Sampsca <sampsca@yahoo.com>
2016-12-07 13:43:03 -0600

    update V2.3

--- bamresize/bamresize.py
+++ bamresize/bamresize.py
@@ -86,13 +86,13 @@ debugging = false
 
 # ===========================================================================
 
-versionStr = "bamresize v2.2"
+versionStr = "bamresize v2.3"
 
 uStr = \
 """Code contributions by Avenger_teambg and Sam.
 
-python bamresize.py [OPTIONS] filename.bam [filename2.bam...]
-Resizes all the frames in filename.bam, creating a new file called flenamer.bam.
+python.exe bamresize.py [OPTIONS] filename.bam [filename2.bam...]
+Resizes all the frames in filename.bam, creating a new file called filenamer.bam.
 The filename(s) to convert can use wildcards.
 
 Options:
@@ -103,16 +103,16 @@ Options:
  '-q PERCENT'
  '--percenth PERCENT'
     Frame heights are resized by PERCENT percent.
-    Default is 75
+    Default is the value for -p (if specified), otherwise it is 75
  '-h'
  '--help'
     Print this message.
 
 examples:
-python bamresize.py MNO3A1.bam
+python.exe bamresize.py MNO3A1.bam
  Resizes all the frames in MNO3A1.bam by 75% and creates MNO3A1r.bam
 
-python bamresize.py -p 50 -q 150 c:\extractedBams\*.bam
+python.exe bamresize.py -p 50 -q 150 c:\extractedBams\*.bam
  Resizes all frame widths by 50% and all frame heights by 150% for all .bam 
  files in the \extractedBams dir.
 """
@@ -627,7 +627,7 @@ def main ():
     #filename = os.path.join(baseDir, filename)
 
     percentw = 75
-    percenth = 75
+    percenth = 0
     
     try:
         opts, args = getopt.getopt(sys.argv[1:],
@@ -647,8 +647,15 @@ def main ():
             sys.exit()
         if o in ("-p", "--percentw"):
 	    percentw = int(a)
+        if percenth == 0:
+	    percenth = int(a)
         if o in ("-q", "--percenth"):
 	    percenth = int(a)
+    if percenth == 0:
+	percenth = 75
+    #print percentw
+    #print percenth
+
 
     if len(args) < 1:
 	usage()
